ef97c4f5126d1e760d7f54420229962bb837cca3
David Charles Edge <David.Edge@nau.edu>
2023-02-06 15:57:32 -0700

    make sure paleo/chron data exists

--- R/lipd2neotoma.R
+++ R/lipd2neotoma.R
@@ -7,6 +7,8 @@
 #' @return neotoma site
 #'
 
+#L <- readLipd("C:/Users/dce25/Downloads/KISIMA [KN1A].lpd")
+#createTSid()
 
 lipd2neotoma <- function(L){
 
@@ -21,139 +23,150 @@ lipd2neotoma <- function(L){
   mtabs1 <- getMeasurementTables(L)
 
 
+  if (sum(grepl("paleo", attributes(mtabs1)$names)) < 1 & sum(grepl("chron", attributes(mtabs1)$names)) < 1){
+    message("No measurement tables found")
+  }
+
+
   #######################################################################
   #paleoData
   #######################################################################
 
   #grab paleoData names and dataframe
-  for (j in 1:sum(grepl("paleo", attributes(mtabs1)$names))){
-    paleoTabIndex <- grep("paleo", attributes(mtabs1)$names)[j]
-    PD1 <- mtabs1[[paleoTabIndex]]
+  if (sum(grepl("paleo", attributes(mtabs1)$names)) > 0){
+    for (j in 1:sum(grepl("paleo", attributes(mtabs1)$names))){
+      paleoTabIndex <- grep("paleo", attributes(mtabs1)$names)[j]
+      PD1 <- mtabs1[[paleoTabIndex]]
 
-    #initiate list for neotoma "samples"
-    allSamps <- list()
+      #initiate list for neotoma "samples"
+      allSamps <- list()
 
-    #iterate over all ages (sample layers)
-    for (k in 1:sum(!is.na(PD1$age))){
-      #cat(k, "\n")
+      #iterate over all ages (sample layers)
+      for (k in 1:sum(!is.na(PD1$age))){
+        #cat(k, "\n")
 
-      sampleTab <- PD1[k,!is.na(PD1[k,])]
+        sampleTab <- PD1[k,!is.na(PD1[k,])]
 
-      sampleTabNames <- names(sampleTab)
+        sampleTabNames <- names(sampleTab)
 
-      pullAgeDepth <- which(sampleTabNames %in% c("age", "depth"))
-      notAgeDepth <- which(!sampleTabNames %in% c("age", "depth"))
+        pullAgeDepth <- which(sampleTabNames %in% c("age", "depth"))
+        notAgeDepth <- which(!sampleTabNames %in% c("age", "depth"))
 
-      neoSamples <- data.frame(matrix(ncol = 10, nrow = length(notAgeDepth), data=NA))
-      colnames(neoSamples) <- c("units", "value", "context", "element", "taxonid", "symmetry", "taxongroup", "elementtype", "variablename", "ecologicalgroup")
+        neoSamples <- data.frame(matrix(ncol = 10, nrow = length(notAgeDepth), data=NA))
+        colnames(neoSamples) <- c("units", "value", "context", "element", "taxonid", "symmetry", "taxongroup", "elementtype", "variablename", "ecologicalgroup")
 
-      rowCt <- 0
-      for (i in notAgeDepth){
-        rowCt <- rowCt + 1
-        units1 <- L$paleoData[[1]]$measurementTable[[1]][sampleTabNames[i]][[1]]$units
-        if(length(units1)>0){
-          neoSamples[rowCt,1] <- units1
-        }
-        element1 <- L$paleoData[[1]]$measurementTable[[1]][sampleTabNames[i]][[1]]$element
-        if(length(element1)>0){
-          neoSamples[rowCt,4] <- element1
-        }
-        taxonid1 <- L$paleoData[[1]]$measurementTable[[1]][sampleTabNames[i]][[1]]$taxonid
-        if(length(taxonid1)>0){
-          neoSamples[rowCt,5] <- taxonid1
-        }
-        taxongroup1 <- L$paleoData[[1]]$measurementTable[[1]][sampleTabNames[i]][[1]]$taxongroup
-        if(length(taxongroup1)>0){
-          neoSamples[rowCt,7] <- taxongroup1
-        }
-        ecologicalgroup1 <- L$paleoData[[1]]$measurementTable[[1]][sampleTabNames[i]][[1]]$ecologicalgroup
-        if(length(ecologicalgroup1)>0){
-          neoSamples[rowCt,10] <- ecologicalgroup1
+        rowCt <- 0
+        for (i in notAgeDepth){
+          rowCt <- rowCt + 1
+          units1 <- L$paleoData[[1]]$measurementTable[[1]][sampleTabNames[i]][[1]]$units
+          if(length(units1)>0){
+            neoSamples[rowCt,1] <- units1
+          }
+          element1 <- L$paleoData[[1]]$measurementTable[[1]][sampleTabNames[i]][[1]]$element
+          if(length(element1)>0){
+            neoSamples[rowCt,4] <- element1
+          }
+          taxonid1 <- L$paleoData[[1]]$measurementTable[[1]][sampleTabNames[i]][[1]]$taxonid
+          if(length(taxonid1)>0){
+            neoSamples[rowCt,5] <- taxonid1
+          }
+          taxongroup1 <- L$paleoData[[1]]$measurementTable[[1]][sampleTabNames[i]][[1]]$taxongroup
+          if(length(taxongroup1)>0){
+            neoSamples[rowCt,7] <- taxongroup1
+          }
+          ecologicalgroup1 <- L$paleoData[[1]]$measurementTable[[1]][sampleTabNames[i]][[1]]$ecologicalgroup
+          if(length(ecologicalgroup1)>0){
+            neoSamples[rowCt,10] <- ecologicalgroup1
+          }
+
+          neoVars <- strsplit(sampleTabNames[i], "_")[[1]]
+          numVars <- length(neoVars)
+          neoSamples[rowCt,2] <- as.integer(sampleTab[i])
+          if(length(grep("undiff", neoVars[1])) > 0){
+            neoVars[1] <- paste0(strsplit(neoVars[1], "undiff")[[1]][1], " undiff.")
+          }
+          if(length(grep("type", neoVars[1])) > 0){
+            neoVars[1] <- paste0(strsplit(neoVars[1], "type")[[1]][1], "-type")
+          }
+          if(numVars == 1){
+            neoSamples[rowCt,9] <- neoVars[1]
+          }else if (numVars == 2){
+            neoSamples[rowCt,9] <- neoVars[1]
+            neoSamples[rowCt,8] <- neoVars[2]
+          }else if (numVars == 3){
+            neoSamples[rowCt,9] <- neoVars[1]
+            neoSamples[rowCt,8] <- neoVars[2]
+            neoSamples[rowCt,3] <- neoVars[3]
+          }
         }
 
-        neoVars <- strsplit(sampleTabNames[i], "_")[[1]]
-        numVars <- length(neoVars)
-        neoSamples[rowCt,2] <- as.integer(sampleTab[i])
-        if(length(grep("undiff", neoVars[1])) > 0){
-          neoVars[1] <- paste0(strsplit(neoVars[1], "undiff")[[1]][1], " undiff.")
-        }
-        if(length(grep("type", neoVars[1])) > 0){
-          neoVars[1] <- paste0(strsplit(neoVars[1], "type")[[1]][1], "-type")
-        }
-        if(numVars == 1){
-          neoSamples[rowCt,9] <- neoVars[1]
-        }else if (numVars == 2){
-          neoSamples[rowCt,9] <- neoVars[1]
-          neoSamples[rowCt,8] <- neoVars[2]
-        }else if (numVars == 3){
-          neoSamples[rowCt,9] <- neoVars[1]
-          neoSamples[rowCt,8] <- neoVars[2]
-          neoSamples[rowCt,3] <- neoVars[3]
-        }
+        neoSamples$element <- neoSamples$elementtype
+        neoSamples <- neoSamples[order(neoSamples$units, neoSamples$value, neoSamples$context, neoSamples$element, as.numeric(neoSamples$taxonid)),]
+
+        ageType <- L$paleoData[[1]]$measurementTable[[1]]$age$units
+        split1 <- strsplit(L$chronData[[1]]$measurementTable[[1]]$ageYoung$TSid, "_")
+        ChronID <- strsplit(split1[[1]][2], "ageYoung")
+
+        ages1 <- data.frame("age" = sampleTab$age,
+                            "agetype" = ageType,
+                            "ageolder" = NA,
+                            "ageyounger" = NA,
+                            "chronologyid" = as.integer(ChronID[[1]][1]),
+                            "chronologyname" = NA,
+                            row.names = 1)
+
+
+        sample1 <- new("sample")
+
+        sample1@datum <- neoSamples
+        sample1@depth <- sampleTab$depth
+        sample1@ages <- ages1
+
+        allSamps[[k]] <- sample1
+
+
       }
 
-      neoSamples$element <- neoSamples$elementtype
-      neoSamples <- neoSamples[order(neoSamples$units, neoSamples$value, neoSamples$context, neoSamples$element, as.numeric(neoSamples$taxonid)),]
-
-      ageType <- L$paleoData[[1]]$measurementTable[[1]]$age$units
-      split1 <- strsplit(L$chronData[[1]]$measurementTable[[1]]$ageYoung$TSid, "_")
-      ChronID <- strsplit(split1[[1]][2], "ageYoung")
-
-      ages1 <- data.frame("age" = sampleTab$age,
-                          "agetype" = ageType,
-                          "ageolder" = NA,
-                          "ageyounger" = NA,
-                          "chronologyid" = as.integer(ChronID[[1]][1]),
-                          "chronologyname" = NA,
-                          row.names = 1)
 
 
-      sample1 <- new("sample")
+      dataset1 <- neotoma2::set_dataset(datasetid = strsplit(L$originalDataUrl, "/")[[1]][length(strsplit(L$originalDataUrl, "/")[[1]])])
 
-      sample1@datum <- neoSamples
-      sample1@depth <- sampleTab$depth
-      sample1@ages <- ages1
+      for (i in 1:length(allSamps)){
+        dataset1@samples@samples[[i]] <- allSamps[[i]]
+      }
 
-      allSamps[[k]] <- sample1
-
+      datasetAll <- new("datasets")
+      datasetAll@datasets[[j]] <- dataset1
 
     }
-
-
-
-    dataset1 <- neotoma2::set_dataset(datasetid = strsplit(L$originalDataUrl, "/")[[1]][length(strsplit(L$originalDataUrl, "/")[[1]])])
-
-    for (i in 1:length(allSamps)){
-      dataset1@samples@samples[[i]] <- allSamps[[i]]
-    }
-
-    datasetAll <- new("datasets")
-    datasetAll@datasets[[j]] <- dataset1
-
   }
+
 
 
   #######################################################################
   #chronData
   #######################################################################
 
-  chronos1 <- new("chronologies")
+  if (sum(grepl("chron", attributes(mtabs1)$names)) > 0){
+    chronos1 <- new("chronologies")
 
-  for (j in 1:sum(grepl("chron", attributes(mtabs1)$names))){
+    for (j in 1:sum(grepl("chron", attributes(mtabs1)$names))){
 
-    chronTabIndex <- grep("chron", attributes(mtabs1)$names)[j]
-    CD1 <- mtabs1[[chronTabIndex]]
+      chronTabIndex <- grep("chron", attributes(mtabs1)$names)[j]
+      CD1 <- mtabs1[[chronTabIndex]]
 
-    CD1 <- CD1[,c(1,3,6,10,7,5,8)]
+      CD1 <- CD1[,c(1,3,6,10,7,5,8)]
 
-    names(CD1) <- c("depth", "thickness", "agelimitolder", "chroncontrolid", "agelimityounger", "chroncontrolage", "chroncontroltype")
+      names(CD1) <- c("depth", "thickness", "agelimitolder", "chroncontrolid", "agelimityounger", "chroncontrolage", "chroncontroltype")
 
-    chronos1@chronologies[[j]] <- new("chronology")
+      chronos1@chronologies[[j]] <- new("chronology")
 
-    chronos1@chronologies[[j]]@chroncontrols <- CD1
+      chronos1@chronologies[[j]]@chroncontrols <- CD1
 
-    chronos1@chronologies[[j]]@chronologyid <- as.integer(ChronID[[1]][1])
+      chronos1@chronologies[[j]]@chronologyid <- as.integer(ChronID[[1]][1])
+    }
   }
+
 
 
 
