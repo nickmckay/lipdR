ee4060506033b9cf6a939f1908cb878e59ad1d48
Nick McKay <nickmckay@users.noreply.github.com>
2024-07-06 21:03:27 -0700

    Delete test-versions.R

--- tests/testthat/test-versions.R
+++
@@ -1,3 +0,0 @@
-test_that("wiki download version change", {
-  expect_s3_class(readLipd("http://wiki.linked.earth/wiki/index.php/Special:WTLiPD?op=export&lipdid=BJ8-03-70GGC.Linsley.2010"),"lipd")
-})
